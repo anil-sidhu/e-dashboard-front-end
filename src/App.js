3364b763c999885b223f2f8de7907ad734899487
anil sidhu <webanilsidhu@gmail.com>
2021-11-15 21:18:40 +0530

    raw navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     <h1>E-Dashboard</h1>
+      <BrowserRouter >
+      <Nav />
+     <Routes>
+       <Route path="/" element={<h1>Product Component</h1>} />
+       <Route path="/add" element={<h1> AddProduct Component</h1>} />
+       <Route path="/update" element={<h1>Update Product Component</h1>} />
+       <Route path="/logout" element={<h1> Logout Component</h1>} />
+       <Route path="/profile" element={<h1>Profile Component</h1>} />
+
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link
+ } from 'react-router-dom';
+const Nav=()=>{
+    return(
+        <div>
+           <ul className="nav-ul">
+               <li><Link to="/">Products</Link></li>
+               <li><Link to="/add">Add Products</Link></li>
+               <li><Link to="/update"> Update Products</Link></li>
+               <li><Link to="/logout">Logout</Link></li>
+               <li><Link to="/profile">Profile</Link></li>
+
+           </ul>
+        </div>
+    )
+}
+
+export default Nav;
