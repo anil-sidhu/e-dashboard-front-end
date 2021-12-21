263d8122b4879d05779242ab40a94e6d8d966c4d
anil sidhu <webanilsidhu@gmail.com>
2021-12-21 20:47:53 +0530

    done with signup api integration

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,23 @@
 import React,{useState} from "react";
+import {useNavigate} from 'react-router-dom'
 
 const SignUp=()=>{
     const [name,setName]=useState("");
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
-    const collectData=()=>{
+    const navigate = useNavigate();
+    const collectData=async ()=>{
         console.warn(name,email,password);
+        let result = await fetch("http://localhost:5000/register",{
+            method:'post',
+            body: JSON.stringify({name,email,password}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        result=await  result.json();
+        console.warn(result);
+        navigate('/')
     }
 
     return(
