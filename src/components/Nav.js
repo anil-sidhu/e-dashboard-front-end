bed6e951785af03b3698397e4d4bcd679b0287a4
anil sidhu <webanilsidhu@gmail.com>
2021-12-27 20:39:11 +0530

    done with nav update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link
+import { Link,useNavigate
  } from 'react-router-dom';
 const Nav=()=>{
     const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout =()=>{
+        localStorage.clear();
+        navigate('/signup')
+    }
     return(
         <div>
            <ul className="nav-ul">
@@ -10,10 +15,8 @@ const Nav=()=>{
                <li><Link to="/add">Add Products</Link></li>
                <li><Link to="/update"> Update Products</Link></li>
                <li><Link to="/profile">Profile</Link></li>
-               <li>{ auth ? <Link to="/logout">Logout</Link> :
+               <li>{ auth ? <Link onClick={logout} to="/signup">Logout</Link> :
                 <Link to="/signup">Sign Up</Link>}</li>
-
-
            </ul>
         </div>
     )
