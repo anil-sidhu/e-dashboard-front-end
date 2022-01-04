9e9f28fee1ae2fa3e7c838b176888ee14de3b2dd
anil sidhu <webanilsidhu@gmail.com>
2022-01-04 20:26:28 +0530

    done with navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import { Link,useNavigate
- } from 'react-router-dom';
-const Nav=()=>{
+import {
+    Link, useNavigate
+} from 'react-router-dom';
+const Nav = () => {
     const auth = localStorage.getItem('user');
     const navigate = useNavigate();
-    const logout =()=>{
+    const logout = () => {
         localStorage.clear();
         navigate('/signup')
     }
-    return(
+    return (
         <div>
-           <ul className="nav-ul">
-               <li><Link to="/">Products</Link></li>
-               <li><Link to="/add">Add Products</Link></li>
-               <li><Link to="/update"> Update Products</Link></li>
-               <li><Link to="/profile">Profile</Link></li>
-                  {
-                      auth ? <li> <Link onClick={logout} to="/signup">Logout</Link></li>
-                      :<>
-                      <li> <Link to="/signup">Sign Up</Link></li>
-                      <li><Link to="/login">Login</Link></li>
-                      </>
-                  }
-           </ul>
+            <img
+            alt='logo'
+            className='logo'
+             src='https://yt3.ggpht.com/ytc/AKedOLR09bCpy_XTq2scU91URc0pWG0EqS_Yc_Zg-r9pBQ=s900-c-k-c0x00ffffff-no-rj' />
+            {
+                auth ?
+
+                    <ul className="nav-ul">
+                        <li><Link to="/">Products</Link></li>
+                        <li><Link to="/add">Add Products</Link></li>
+                        <li><Link to="/update"> Update Products</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li> <Link onClick={logout} to="/signup">Logout ({ JSON.parse(auth).name})</Link></li>
+                    </ul>
+                    :
+                    <ul className="nav-ul nav-right">
+                        <li> <Link to="/signup">Sign Up</Link></li>
+                        <li><Link to="/login">Login</Link></li>
+                    </ul>
+            }
+
+
         </div>
     )
 }
